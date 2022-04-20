9a5401a14cf5d3c93fd897980a6158d803efa401
Tu Thuc <Admin@DESKTOP-2RT7I3F>
2022-04-20 23:08:18 +0700

    Merge branch 'master' of https://github.com/TuThuc/hybrid-framework-nopcomerce

--- javaTest/javabasic/Topic_03_For_Foreach.java
+++ javaTest/javabasic/Topic_03_For_Foreach.java
@@ -18,7 +18,7 @@ for(int i= 1; i<=number; i ++) {
 }
 			
 	}
-@Test
+//@Test
 public void TC_02() {
 	int numberA = scanner.nextInt();
 int numberB = scanner.nextInt();
@@ -26,7 +26,21 @@ for(int i= numberA; i<= numberB; i++) {
 	System.out.print(" " +i);
 }
 }
-	
+@Test
+public void TC_03() {
+	int mounth = scanner.nextInt();
+//int numberB = scanner.nextInt();
+switch(mounth)
+{case 1:{
+	System.out.print("JAN ");	
+	break;
+}
+case 2:{
+	System.out.print("FE ");
+	break;
+}
+}	
+}
 }
 
 
--- javaTest/javabasic/Topic_04_Array.java
+++ javaTest/javabasic/Topic_04_Array.java
@@ -16,7 +16,7 @@ public class Topic_04_Array {
 		System.out.println("phan tu lon nhat mang la:" + max);
 	}
 
-	@Test
+	//@Test
 
 	public void TC_02() {
 		int arr[] = { 2, 7, 6, 8, 9 };
@@ -28,5 +28,81 @@ public class Topic_04_Array {
 		}
 		System.out.println("tong cua mang:" + tong);
 	}
+	//@Test
 
+	public void TC_03() {
+		int arr[] = { 2, 7, 6, 8, 9 };
+		int tong = 0;
+		for (int i = 0; i < arr.length; i++) {
+
+			tong = arr[0]+ arr[arr.length-1];
+
+		}
+		System.out.println("tong cua phan tu dau va phan tu cuoi:" + tong);
+	}
+
+	//@Test
+
+	public void TC_04() {
+		int arr[] = { 2, 7, 6, 8, 9, 16, 17, 20, 21 };
+		// int tong = 0;
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] % 2 == 0) {
+				System.out.println(arr[i]);
+			}
+
+		}
+	}
+	//@Test
+
+	public void TC_05() {
+		int arr[] = { 3, -7, 2, 5, 9, -6, 10, 12 };
+		int tong = 0;
+		for (int i = 0; i < arr.length; i++) {
+
+			if ( arr[i] > 0 && arr[i] % 2 != 0) {
+			System.out.println(arr[i]);	
+			tong +=arr[i];
+			}
+			
+		}
+		System.out.println(tong);
+	}
+	//@Test
+
+	public void TC_06() {
+		int arr[] = { 3, -7, 2, 5, 9, -6, 10, 12 };
+		//int tong = 0;
+		for (int i = 0; i < arr.length; i++) {
+
+			if ( arr[i] >= 0 && arr[i] <= 10) {
+			System.out.println(arr[i]);	
+			//tong +=arr[i];
+			}
+			
+		}
+		//System.out.println(a);
+	}
+	@Test
+
+	public void TC_07() {
+		int arr[] = { 3, 5, 7, 30, 10, 5, 8, 23,0,-5 };
+		int tong = 0;
+		float tbc = 0;
+		for (int i = 0; i < arr.length; i++) {
+
+			tong +=arr[i];
+				
+			//tong +=arr[i];
+			
+			
+			}
+		System.out.println(tong);
+		tbc= tong / arr.length;
+		
+		System.out.println(tbc);
+		}
+		//System.out.println(a);
+	
 }
