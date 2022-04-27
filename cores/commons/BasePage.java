2e38556b86dea4e689c4f70da8f5915cd5e235b7
Tu Thuc <Tu Thuc@thuctv>
2022-04-27 18:02:54 +0700

    Framework 03- Build BasePage class

--- cores/commons/BasePage.java
+++ cores/commons/BasePage.java
@@ -1,5 +1,128 @@
 package commons;
 
-public class BasePage {
+import java.util.Set;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//Common class
+public class BasePage {
+	WebDriver driver;
+
+	// Nhiệm vụ mở 1 URL bất kỳ
+	public void openPageUrl(WebDriver driver, String pageUrl) {
+		driver.get(pageUrl);
+	}
+
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+
+	public String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+
+	public String getPageSourceCode(WebDriver driver) {
+		return driver.getPageSource();
+	}
+
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+
+	public void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+
+	public Alert waitForAlertPresence(WebDriver driver) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, 30);
+		return explicitWait.until(ExpectedConditions.alertIsPresent());
+
+	}
+
+	public void acceptAlert(WebDriver driver) {
+		waitForAlertPresence(driver).accept();
+		;
+
+	}
+
+	public void cancelAlert(WebDriver driver) {
+		waitForAlertPresence(driver).dismiss();
+	}
+
+	public String getAlertText(WebDriver driver) {
+		return waitForAlertPresence(driver).getText();
+	}
+
+	public void sendkeyToAlert(WebDriver driver, String textValue) {
+		waitForAlertPresence(driver).sendKeys(textValue);
+	}
+
+	public void switchToWindowByID(WebDriver driver, String expectedID) {
+		Set<String> allTabIDs = driver.getWindowHandles();
+
+		for (String id : allTabIDs) {
+
+			if (!id.equals(expectedID)) {
+				driver.switchTo().window(id);
+				break;
+			}
+		}
+	}
+
+	public void switchToWindowByTitle(WebDriver driver, String expectedTitle) {
+
+		Set<String> allTabIDs = driver.getWindowHandles();
+
+		for (String id : allTabIDs) {
+
+			driver.switchTo().window(id);
+
+			String actualTitle = driver.getTitle();
+
+			if (actualTitle.equals(expectedTitle)) {
+				break;
+			}
+		}
+	}
+
+	public void closeAllTabWithoutParent(WebDriver driver, String parentID) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		for (String id : allWindowIDs) {
+			if (!id.equals(parentID)) {
+				driver.switchTo().window(id);
+				driver.close();
+			}
+			driver.switchTo().window(parentID);
+		}
+
+	}
+
+	public WebElement getWebElement(WebDriver driver, String xpathLocator) {
+		return driver.findElement(By.xpath(xpathLocator));
+	}
+
+	public void clickToElement(WebDriver driver, String xpathLocator) {
+		getWebElement(driver, xpathLocator).click();
+	}
+
+	public void sendkeyToElement(WebDriver driver, String xpathLocator, String textValue) {
+		WebElement element = getWebElement(driver, xpathLocator);
+		element.clear();
+		element.sendKeys(textValue);
+	}
+
+	public void getElementText(WebDriver driver, String xpathLocator) {
+		getWebElement(driver, xpathLocator).getText();
+	}
+
+	public void selectItemInDefaultDropdown(WebDriver driver, String xpathLocator, String textValue) {
+		Select select = new Select(getWebElement(driver, xpathLocator));
+		select.selectByValue(textValue);
+	}
 }
