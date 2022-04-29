9c5f640edc5a7bb1001ab32c69634fe0999a86af
Tu Thuc <Tu Thuc@thuctv>
2022-04-29 17:39:53 +0700

    Framework 05- Page Object Pattern - Part I(continue)

--- testcases/com/nopcomerce/user/Level_03__Page_Object.java
+++ testcases/com/nopcomerce/user/Level_03__Page_Object.java
@@ -14,7 +14,7 @@ import commons.BasePage;
 import pageObjects.HomePageObject;
 import pageObjects.RegisterPageObject;
 
-public class Level_03__Page_Object extends BasePage {
+public class Level_03__Page_Object  {
 	WebDriver driver;
 	String emailAdress;
 
